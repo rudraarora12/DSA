bbbbfcc703920d1250342127deea0725ea2dae9a
rudraarora12 <rudraarora802@gmail.com>
2026-08-18 17:17:57 +0530

    Initial commit: Added IntelliGrid frontend codebase

---
+++ binaryseARCH.java
@@ -0,0 +1,7 @@
+public class binaryseARCH {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
---
+++ sorting/InsertionSort.java
@@ -0,0 +1,55 @@
+package sorting;
+// public class Main {
+//     public static void print(int[] arr){
+//         for(int ele:arr){
+//             System.out.println(ele+" ");
+//         }
+//         System.out.println();
+        
+//     }
+//     // SELECTION SORT
+//     // public static void main(String[] args) {
+//     //     int[] arr = {5,-2,0,3,9,10};
+//     //     int n = arr.length;
+//     //     print(arr);
+//     //     for(int i = 0; i<n-1;i++){
+//     //         for(int j = i+1; j<n; j++){
+//     //             if(arr[i]>arr[j]){
+//     //                 int temp = arr[i];
+//     //                 arr[i] = arr[j];
+//     //                 arr[j] = temp;
+//     //             }
+//     //         }   
+//     //     }
+//     //     print(arr);
+
+
+        
+//     // }
+//     // BUBBLE SORT(optimized)
+//     public static void main(String[] args){
+//         int[] arr = {2,4,1,0,-3,0};
+//         int n = arr.length;
+//         print(arr);
+//         for(int i = 0; i<n-1; i++){
+//             boolean swaap = false;
+//             for(int j =0; j<n-1-i; j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                     swaap = true;
+//                 }
+
+//             }
+//             if(!swaap){
+//                 break;
+//             }
+
+//         }
+//         System.out.println();
+//         print(arr);
+        
+  
+//     }
+// }
